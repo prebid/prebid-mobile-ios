12d66e9df175e0fdcef3365a840365ef84ca589a
Nicole Hedley <nhedley@appnexus.com>
2017-07-07 14:35:36 -0400

    Add encrypted provisioning profile, updated scripts to build ipa on Travis CI

--- scripts/testPrebidMobile.sh
+++ scripts/testPrebidMobile.sh
@@ -6,10 +6,23 @@ cd ../sdk/
 
 echo "Running unit tests"
 gem install xcpretty
-xcodebuild test -project PrebidMobile.xcodeproj -scheme "PrebidMobileTests" -destination 'platform=iOS Simulator,id=$IOS_SIMULATOR_UDID' | xcpretty
+xcodebuild test -project PrebidMobile.xcodeproj -scheme "PrebidMobileTests" -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' | xcpretty
+
+echo "Building ipa"
+cd ../examples/PrebidMobileDemo/
+
+# Make the keychain the default so identities are found
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u ios-build.keychain
+
+make ipa
 
 echo "Running integration tests"
-cd ../examples/PrebidMobileDemo/
-#make ipa
+
 gem install xamarin-test-cloud
 test-cloud submit Products/ipa/PrebidMobileDemo.ipa 435c130f3f6ff5256d19a790c21dd653 --devices 9f82ba1c --series "master" --locale "en_US" --app-name "AppNexus.PrebidMobileDemo" --user nhedley@appnexus.com
