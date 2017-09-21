10711bdac4213dc77a3f0a48a130dfe3905272f8
Nicole Hedley <nhedley@appnexus.com>
2017-09-21 13:59:01 -0400

    Add ip address to PBS request and fix geo targeting bug

--- sdk/PrebidServerAdapter/PBServerAdapter.m
+++ sdk/PrebidServerAdapter/PBServerAdapter.m
@@ -13,9 +13,11 @@
  limitations under the License.
  */
 
+#import <arpa/inet.h>
 #import <AdSupport/AdSupport.h>
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <ifaddrs.h>
 #import <sys/utsname.h>
 #import <UIKit/UIKit.h>
 
@@ -248,6 +250,7 @@ static NSString *const kPrebidMobileVersion = @"0.0.3";
     if (deviceId) {
         deviceDict[@"ifa"] = deviceId;
     }
+    deviceDict[@"ip"] = [self getIPAddress];
     
     NSInteger timeInMiliseconds = (NSInteger)[[NSDate date] timeIntervalSince1970];
     deviceDict[@"devtime"] = @(timeInMiliseconds);
@@ -255,9 +258,39 @@ static NSString *const kPrebidMobileVersion = @"0.0.3";
     return [deviceDict copy];
 }
 
+// Get IP Address
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+}
+
 - (NSDictionary *)geo {
     CLLocation *clLocation = [[PBTargetingParams sharedInstance] location];
-    PBServerLocation *location = [PBServerLocation getLocationWithLatitude:clLocation.coordinate.latitude longitude:clLocation.coordinate.longitude timestamp:clLocation.timestamp horizontalAccuracy:clLocation.horizontalAccuracy];
+    PBServerLocation *location;
+    if (clLocation) {
+        location = [PBServerLocation getLocationWithLatitude:clLocation.coordinate.latitude longitude:clLocation.coordinate.longitude timestamp:clLocation.timestamp horizontalAccuracy:clLocation.horizontalAccuracy];
+    }
     if (location) {
         NSMutableDictionary *geoDict = [[NSMutableDictionary alloc] init];
         CGFloat latitude = location.latitude;
