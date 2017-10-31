5e61122b881638010720fe2d6a3af617c3a30776
Nicole Hedley <nhedley@appnexus.com>
2017-10-31 14:20:28 -0400

    edit header files

--- sdk/PrebidDemandSDKLoaderForDFP/PrebidDemandSDKLoaderForDFP.h
+++ sdk/PrebidDemandSDKLoaderForDFP/PrebidDemandSDKLoaderForDFP.h
@@ -23,5 +23,5 @@ FOUNDATION_EXPORT double PrebidDemandSDKLoaderForDFPVersionNumber;
 FOUNDATION_EXPORT const unsigned char PrebidDemandSDKLoaderForDFPVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PrebidDemandSDKLoaderForDFP/PublicHeader.h>
-
-
+#import <PrebidDemandSDKLoaderForDFP/PrebidCustomEventBannerDFP.h>
+#import <PrebidDemandSDKLoaderForDFP/PrebidCustomEventInterstitialDFP.h>
--- sdk/PrebidDemandSDKLoaderForMoPub/PrebidDemandSDKLoaderForMoPub.h
+++ sdk/PrebidDemandSDKLoaderForMoPub/PrebidDemandSDKLoaderForMoPub.h
@@ -1,10 +1,17 @@
-//
-//  PrebidDemandSDKLoaderForMoPub.h
-//  PrebidDemandSDKLoaderForMoPub
-//
-//  Created by Nicole Hedley on 10/25/17.
-//  Copyright © 2017 Nicole Hedley. All rights reserved.
-//
+/*   Copyright 2017 APPNEXUS INC
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 
@@ -15,5 +22,5 @@ FOUNDATION_EXPORT double PrebidDemandSDKLoaderForMoPubVersionNumber;
 FOUNDATION_EXPORT const unsigned char PrebidDemandSDKLoaderForMoPubVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PrebidDemandSDKLoaderForMoPub/PublicHeader.h>
-
-
+#import <PrebidDemandSDKLoaderForMoPub/PrebidCustomEventBannerMoPub.h>
+#import <PrebidDemandSDKLoaderForMoPub/PrebidCustomEventInterstitialMoPub.h>
