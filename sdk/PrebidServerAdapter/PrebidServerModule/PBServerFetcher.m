76c1a52cbd429ac7f08c670aee4b871a35d128fe
Sandra Leon <sleon@rubiconproject.com>
2017-11-27 14:05:05 -0800

    Updated copyright message in all files

--- sdk/PrebidServerAdapter/PrebidServerModule/PBServerFetcher.m
+++ sdk/PrebidServerAdapter/PrebidServerModule/PBServerFetcher.m
@@ -1,5 +1,5 @@
 
-/*   Copyright 2017 APPNEXUS INC
+/*   Copyright 2017 Prebid.org, Inc.
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
