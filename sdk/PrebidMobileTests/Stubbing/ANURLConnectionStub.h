72456237596046f334ed30b9fe293f765b88b10e
Sandra Leon <sleon@rubiconproject.com>
2017-12-18 13:24:53 -0800

    Reset copyright header files. will create new pr for that

--- sdk/PrebidMobileTests/Stubbing/ANURLConnectionStub.h
+++ sdk/PrebidMobileTests/Stubbing/ANURLConnectionStub.h
@@ -1,4 +1,4 @@
-/*   CCopyright 2017 Prebid.org, Inc.
+/*   CCopyright 2017 APPNEXUS INC
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
