3b96e764103f4f730f325f43bc27b5ebf033cb92
Wei Zhang <wzhang@appnexus.com>
2018-04-23 14:22:39 -0400

    add email config feature for 3rd test

--- tools/PrebidMobileValidator/PrebidMobileValidator/PrebidSDKTesting/BannerViewControllers/BannerTestsViewController.m
+++ tools/PrebidMobileValidator/PrebidMobileValidator/PrebidSDKTesting/BannerViewControllers/BannerTestsViewController.m
@@ -18,8 +18,9 @@
 #import <GoogleMobileAds/DFPBannerView.h>
 #import "MPAdView.h"
 #import "PrebidMobile/PrebidMobile.h"
+#import <MessageUI/MFMailComposeViewController.h>
 
-@interface BannerTestsViewController () <GADBannerViewDelegate, MPAdViewDelegate>
+@interface BannerTestsViewController () <GADBannerViewDelegate, MPAdViewDelegate, MFMailComposeViewControllerDelegate>
 
 @property (strong, nonatomic) MPAdView *mopubAdView;
 @property (strong, nonatomic) DFPBannerView *dfpAdView;
@@ -39,7 +40,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    // todo add see integration code button
+    [self setupEmailConfigButton];
+}
+
+-(void) setupEmailConfigButton
+{
+    UIBarButtonItem *emailConfig = [[UIBarButtonItem alloc]init];
+    emailConfig.style = UIBarButtonItemStylePlain;
+    emailConfig.title = @"Email Config";
+    emailConfig.target = self;
+    emailConfig.action = NSSelectorFromString(@"emailConfig:");
+    self.navigationItem.rightBarButtonItem = emailConfig;
+}
+
+- (void) emailConfig:(id) sender
+{
+    if([MFMailComposeViewController canSendMail]){
+        MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+        mailViewController.mailComposeDelegate = self;
+        [mailViewController setSubject:@"Step by step instruction to setup your Prebid Mobile"];
+        NSMutableString *body = [[NSMutableString alloc]initWithString:@""];
+        [body appendString: @"Hi, \n\n"];
+        [body appendString:@"Please follow the instructions below to set up your Prebid Mobile.\n\n"];
+        [body appendString:@"Step 1: as early as possible in the lifecycle of your app, create the ad unit and register with Prebid Mobile: \n"];
+        NSString *configId = [[NSUserDefaults standardUserDefaults] stringForKey:kPBConfigKey];
+        NSString *accountId = [[NSUserDefaults standardUserDefaults] stringForKey:kPBAccountKey];
+        [body appendString:[NSString stringWithFormat: @"PBBannerAdUnit *adUnit = [[PBBannerAdUnit alloc]initWithAdUnitIdentifier:@\"Home\" andConfigId:@\"%@\"];\n", configId]];
+        [body appendString:@"[adUnit addSize: CGSizeMake(300, 250)];\n"];
+        [body appendString:@"NSArray *adUnits = [NSArray arrayWithObjects:adUnit, nil];\n"];
+        [body appendString:[NSString stringWithFormat:@"[PrebidMobile registerAdUnits:adUnits withAccountId:@\"%@\" withHost:PBServerHostAppNexus andPrimaryAdServer:PBPrimaryAdServerMoPub];", accountId]];
+        [body appendString:@"\n\nStep 2: create your banner ad view and apply prebid bids on the object:\n"];
+        NSString *adUnitID = [[NSUserDefaults standardUserDefaults] stringForKey:kAdUnitIdKey];
+        [body appendString:[NSString stringWithFormat:@"MPAdView *mopubAdView = [[MPAdView alloc] initWithAdUnitId:@\"%@\" size:CGSizeMake(300, 250)];\n", adUnitID]];
+        [body appendString:@"Configure your mopubAdView...\n"];
+        [body appendString:[NSString stringWithFormat:@"[PrebidMobile setBidKeywordsOnAdObject:mopubAdView withAdUnitId:@\"Home\" withTimeout:600 completionHandler:^{[mopubAdView loadAd];}];"]];
+        [mailViewController setMessageBody:body isHTML:NO];
+        [self presentViewController:mailViewController animated:YES completion:nil];
+    } else {
+        UIAlertController *alert = [[UIAlertController alloc] init];
+        alert.title = @"Uable to send email";
+        alert.message = @"Please set up an email account on your device.";
+        UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:alertAction];
+        [self presentViewController: alert animated:YES completion:nil];
+    }
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [controller dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
