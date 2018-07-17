ca17509a743cbc7648029254e26c0e19a5be4102
Wei Zhang <wzhang@appnexus.com>
2018-07-17 16:51:04 -0400

    break out docs string

--- tools/PrebidValidator/Dr.Prebid/Settings/HelpViewController.m
+++ tools/PrebidValidator/Dr.Prebid/Settings/HelpViewController.m
@@ -28,15 +28,22 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     WKWebView *content = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    NSMutableString *htmlString = [[NSMutableString alloc] init];
+    [htmlString appendString:@"<head>"];
+    [htmlString appendString:@"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"];
+    // TODO: add style sheet
+    [htmlString appendString:@"</head><body>"];
     if ([self.title isEqualToString:kAboutString]) {
-        [content loadHTMLString:@"<body>Let me tell you what is Dr.Prebid</body>" baseURL:nil];
+        [htmlString appendString:@"Let me tell you what is Dr.Prebid."];
     } else if( [self.title isEqualToString:kGeneralInfoHelpString]) {
-        [content loadHTMLString:@"<body>Let me tell you what is General Info.</body>" baseURL:nil];
+        [htmlString appendString:@"Let me tell you what is General Info."];
     } else if ( [self.title isEqualToString:kAdServerInfoHelpString]) {
-        [content loadHTMLString:@"<body>Let me tell you what is Ad Server Info.</body>" baseURL:nil];
+        [htmlString appendString:@"Let me tell you what is Ad Server Info."];
     } else if ( [self.title isEqualToString:kPrebidServerInfoHelpString]) {
-        [content loadHTMLString:@"<body>Let me tell you what is Prebid Server Info.</body>" baseURL:nil];
+        [htmlString appendString:@"Let me tell you what is Prebid Server Info."];
     }
+    [htmlString appendString:@"</body>"];
+    [content loadHTMLString:htmlString baseURL:nil];
     [self.view addSubview:content];
 }
 
@@ -44,15 +51,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
