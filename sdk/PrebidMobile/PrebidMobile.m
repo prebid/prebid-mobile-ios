1a4b571ee7e4a4413aee0ca2e2cc84eb8fc6503d
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2018-07-25 19:33:07 +0800

    Update PrebidMobile.m

--- sdk/PrebidMobile/PrebidMobile.m
+++ sdk/PrebidMobile/PrebidMobile.m
@@ -21,8 +21,9 @@
 + (void)registerAdUnits:(nonnull NSArray<PBAdUnit *> *)adUnits
           withAccountId:(nonnull NSString *)accountId
                withHost:(PBServerHost)host
+   withPriceGranularity:(PBPriceGranularity)priceGranularity
      andPrimaryAdServer:(PBPrimaryAdServerType)adServer {
-    [[PBBidManager sharedInstance] registerAdUnits:adUnits withAccountId:accountId withHost:host andPrimaryAdServer:adServer];
+    [[PBBidManager sharedInstance] registerAdUnits:adUnits withAccountId:accountId withHost:host withPriceGranularity:priceGranularity andPrimaryAdServer:adServer ];
 }
 
 + (void)setBidKeywordsOnAdObject:(nonnull id)adObject
