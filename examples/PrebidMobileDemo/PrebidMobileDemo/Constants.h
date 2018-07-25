a467d5286dcd2fdbfec92d2e45dbc9ddb256a53f
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2018-07-25 18:49:08 +0800

    Fix price granularity

--- examples/PrebidMobileDemo/PrebidMobileDemo/Constants.h
+++ examples/PrebidMobileDemo/PrebidMobileDemo/Constants.h
@@ -46,6 +46,7 @@ extern NSString *const kAccountId;
 extern NSString *const kAdUnit1ConfigId;
 extern NSString *const kAdUnit2ConfigId;
 extern NSUInteger const kPBServerHost;
+extern NSUInteger const kPPriceGranularity;
 
 extern NSString *const kAdUnit1Id;
 extern NSString *const kAdUnit2Id;
