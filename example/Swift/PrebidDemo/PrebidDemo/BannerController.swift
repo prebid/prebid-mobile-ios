549c1c039640b5be7aad2b200f64a70b5d737178
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2019-07-04 17:51:56 +0300

    small refactoring

--- example/Swift/PrebidDemo/PrebidDemo/BannerController.swift
+++ example/Swift/PrebidDemo/PrebidDemo/BannerController.swift
@@ -109,16 +109,17 @@ class BannerController: UIViewController, GADBannerViewDelegate, MPAdViewDelegat
     func adViewDidReceiveAd(_ bannerView: GADBannerView) {
         print("adViewDidReceiveAd")
         
-        Utils.shared.findPrebidCreativeSize(bannerView, success: { (size) in
-            guard let bannerView = bannerView as? DFPBannerView else {
-                return
-            }
-            
-            bannerView.resize(GADAdSizeFromCGSize(size))
-            
-        }) { (error) in
-            print("error: \(error.localizedDescription)");
-        }
+        Utils.shared.findPrebidCreativeSize(bannerView,
+                                            success: { (size) in
+                                                guard let bannerView = bannerView as? DFPBannerView else {
+                                                    return
+                                                }
+                                                
+                                                bannerView.resize(GADAdSizeFromCGSize(size))},
+                                            failure: { (error) in
+                                                print("error: \(error.localizedDescription)");
+                                                
+        })
     }
 
     func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
