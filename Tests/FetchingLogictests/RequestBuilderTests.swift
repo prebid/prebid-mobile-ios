c00f8776e6585335ba8b070c974b1903364bec6a
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2019-10-07 20:14:59 +0300

    ifa fix

--- Tests/FetchingLogictests/RequestBuilderTests.swift
+++ Tests/FetchingLogictests/RequestBuilderTests.swift
@@ -970,7 +970,8 @@ class RequestBuilderTests: XCTestCase, CLLocationManagerDelegate {
             if (carrier?.carrierName?.count ?? 0) > 0 {
                 XCTAssertEqual(carrier?.carrierName ?? "", device["carrier"] as! String)
             }
-            XCTAssertEqual(RequestBuilder.DeviceUUID(), device["ifa"] as! String)
+            let ifa = device["ifa"] as? String ?? ""
+            XCTAssertEqual(RequestBuilder.DeviceUUID(), ifa)
             let lmtAd: Bool = !ASIdentifierManager.shared().isAdvertisingTrackingEnabled
             XCTAssertEqual(NSNumber(value: lmtAd).intValue, device["lmt"] as! Int)
             XCTAssertEqual(UIScreen.main.scale, device["pxratio"] as! CGFloat)
