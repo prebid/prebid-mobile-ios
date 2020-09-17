b329d673de53656d49386b86c820351d84948ba4
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2020-09-17 17:52:12 +0300

    fixed merge conflict

--- Tests/AdUnitTests/AdUnitSuccessorObjCTests.m
+++ Tests/AdUnitTests/AdUnitSuccessorObjCTests.m
@@ -107,7 +107,7 @@ NSString * const configId = @"1001-1";
 - (void)testVideoParametersCreation {
     
     //given
-    VideoAdUnit *videoAdUnit = [[VideoAdUnit alloc] initWithConfigId:@"6ace8c7d-88c0-4623-8117-75bc3f0a2e45" size:CGSizeMake(300, 250) type: PBVideoPlacementTypeInBanner];
+    VideoAdUnit *videoAdUnit = [[VideoAdUnit alloc] initWithConfigId:@"6ace8c7d-88c0-4623-8117-75bc3f0a2e45" size:CGSizeMake(300, 250)];
     VideoInterstitialAdUnit *videoInterstitialAdUnit = [[VideoInterstitialAdUnit alloc] initWithConfigId:@"6ace8c7d-88c0-4623-8117-75bc3f0a2e45"];
     RewardedVideoAdUnit *rewardedVideoAdUnit = [[RewardedVideoAdUnit alloc] initWithConfigId:@"6ace8c7d-88c0-4623-8117-75bc3f0a2e45"];
 
