28351ec79c164657e541776f13cf4a749423acac
mobileSDK <ppuviarasu@appnexus.com>
2020-11-10 14:01:38 -0500

    updated adtag url

--- Source/Addendum/IMAUtils.swift
+++ Source/Addendum/IMAUtils.swift
@@ -1,10 +1,17 @@
-//
-//  IMAUtils.swift
-//  PrebidMobile
-//
-//  Created by Punnaghai Puviarasu on 10/29/20.
-//  Copyright © 2020 AppNexus. All rights reserved.
-//
+/*   Copyright 2018-2019 Prebid.org, Inc.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import Foundation
 
@@ -17,7 +24,7 @@ public final class IMAUtils: NSObject {
     
     @objc
     public func constructAdTagURLForIMAWithPrebidKeys (adUnitID:String, customKeywords: [String:String]) -> String{
-        let adServerURL = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&output=vast&unviewed_position_start=1&gdfp_req=1&env=vp"
+        let adServerURL = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480|400x300&output=xml_vast4&unviewed_position_start=1&gdfp_req=1&env=vp"
         let adUnit = String(format: "iu=%@", adUnitID)
         
         let andString: String = "&"
