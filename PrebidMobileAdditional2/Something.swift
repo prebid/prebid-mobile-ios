17c5f3f4c76440fffeeb56e563356c998626e27e
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2021-04-26 15:31:51 +0300

    - SPM some experiments

---
+++ PrebidMobileAdditional2/Something.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex on 24.04.2021.
+//
+
+import UIKit
+
+
+public class Something {
+    public static func foo() {
+        let l = CGSize()
+    }
+}
