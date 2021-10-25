1373a8766546f205e4d2ef10dfac27800e7b76fd
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2021-10-25 13:50:22 +0300

    * feat: Add fidelity type to bid response

--- PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRendering/Prebid/PBMCore/ORTB/Prebid/PBMORTBBidExtSkadn.m
+++ PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRendering/Prebid/PBMCore/ORTB/Prebid/PBMORTBBidExtSkadn.m
@@ -14,9 +14,9 @@
  */
 
 #import <Foundation/Foundation.h>
+#import <StoreKit/StoreKit.h>
 
 #import "PBMORTBAbstract+Protected.h"
-
 #import "PBMORTBBidExtSkadn.h"
 
 @implementation PBMORTBBidExtSkadn
@@ -31,7 +31,7 @@
         _sourceapp = jsonDictionary[@"sourceapp"];
         _timestamp = jsonDictionary[@"timestamp"];
         _signature = jsonDictionary[@"signature"];
-        
+        _fidelities = jsonDictionary[@"fidelities"];
     }
     return self;
 }
@@ -47,10 +47,67 @@
     ret[@"sourceapp"] = self.sourceapp;
     ret[@"timestamp"] = self.timestamp;
     ret[@"signature"] = self.signature;
-    
+    ret[@"fidelities"] = self.fidelities;
     [ret pbmRemoveEmptyVals];
     
     return ret;
+}
+
+- (NSDictionary<NSString *, id> * _Nullable) getSkadnInfo {
+    if (@available(iOS 14.0, *)) {
+        NSDictionary<NSString * , id> *productParams = [NSDictionary alloc];
+        
+        if (self.itunesitem != nil &&
+            self.network != nil &&
+            self.campaign != nil &&
+            self.version != nil &&
+            self.sourceapp != nil) {
+            [productParams setValue:SKStoreProductParameterITunesItemIdentifier forKey:self.itunesitem.stringValue];
+            [productParams setValue:SKStoreProductParameterAdNetworkIdentifier forKey:self.network];
+            [productParams setValue:SKStoreProductParameterAdNetworkCampaignIdentifier forKey:self.campaign.stringValue];
+            [productParams setValue:SKStoreProductParameterAdNetworkVersion forKey:self.version];
+            [productParams setValue:SKStoreProductParameterAdNetworkSourceAppStoreIdentifier forKey:self.sourceapp.stringValue];
+            
+            if(self.timestamp != nil &&
+               self.nonce != nil &&
+               self.signature != nil) {
+                [productParams setValue:SKStoreProductParameterAdNetworkTimestamp forKey:self.timestamp.stringValue];
+                [productParams setValue:SKStoreProductParameterAdNetworkNonce forKey:self.nonce.UUIDString];
+                [productParams setValue:SKStoreProductParameterAdNetworkAttributionSignature forKey:self.signature];
+            }
+            
+            return productParams;
+        }
+    }
+    return nil;
+}
+
+- (NSDictionary<NSString *, id> * _Nullable) getSkadnInfoForFidelityType:(NSNumber *) fidelityType {
+    NSDictionary<NSString *, id> * _Nullable productParams = self.getSkadnInfo;
+    if (@available(iOS 14.5, *)) {
+        if (self.fidelities != nil) {
+            for(PBMORTBSkadnFidelity *fid in self.fidelities) {
+                if ([fid.fidelity isEqual:fidelityType]) {
+                    [productParams setValue:SKStoreProductParameterAdNetworkTimestamp forKey: fid.timestamp.stringValue];
+                    [productParams setValue:SKStoreProductParameterAdNetworkNonce forKey: fid.nonce.UUIDString];
+                    [productParams setValue:SKStoreProductParameterAdNetworkAttributionSignature forKey: fid.signature];
+                }
+            }
+        }
+    }
+    
+    return productParams;
+}
+
+- (NSArray<NSNumber *> * _Nullable) getFidelityTypes {
+    if (self.fidelities != nil) {
+        NSMutableArray<NSNumber *> *fids = [[NSMutableArray alloc] initWithCapacity:2];
+        for(PBMORTBSkadnFidelity *fid in self.fidelities) {
+            [fids addObject:fid.fidelity];
+            return fids;
+        }
+    }
+    return  nil;
 }
 
 @end
