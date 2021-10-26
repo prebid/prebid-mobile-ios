bec0a72f0233d9d3b2357b246da4dd93b095d709
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2021-10-26 16:09:32 +0300

    *refactor: Refactor SkadnParametersManager

--- PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRendering/SkadnParametersManager.swift
+++ PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRendering/SkadnParametersManager.swift
@@ -1,18 +1,18 @@
 //
 /*   Copyright 2018-2021 Prebid.org, Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  */
 
 import Foundation
 import StoreKit
@@ -20,48 +20,36 @@ import StoreKit
 @objcMembers
 public class SkadnParametersManager: NSObject {
     
-    private static func getFidelityTypes(for skadnInfo: PBMORTBBidExtSkadn) -> Array<NSNumber>? {
+    private static func getFidelity(from skadnInfo: PBMORTBBidExtSkadn, fidelityType: NSNumber) -> PBMORTBSkadnFidelity? {
         guard let fidelities = skadnInfo.fidelities else { return nil }
-        var fidsArray = [NSNumber]()
-        for fid in fidelities {
-            if let safeFid = fid.fidelity {
-                fidsArray.append(safeFid)
+        
+        for fidelity in fidelities {
+            if fidelity.fidelity == fidelityType {
+                return fidelity
             }
         }
-        return fidsArray
+        return nil
     }
     
     @available(iOS 14.5, *)
     public static func getSkadnImpression(for skadnInfo: PBMORTBBidExtSkadn) -> SKAdImpression? {
-        if let fids = SkadnParametersManager.getFidelityTypes(for: skadnInfo) {
-            if fids.contains(0) {
-                let imp = SKAdImpression()
-                if let itunesitem = skadnInfo.itunesitem,
-                   let network = skadnInfo.network,
-                   let campaign = skadnInfo.campaign,
-                   let sourceapp = skadnInfo.sourceapp {
-                    imp.sourceAppStoreItemIdentifier = sourceapp
-                    imp.advertisedAppStoreItemIdentifier = itunesitem
-                    imp.adNetworkIdentifier = network
-                    imp.adCampaignIdentifier = campaign
-                    
-                    if let safeFids = skadnInfo.fidelities {
-                        for fid in safeFids {
-                            if fid.fidelity == 0 {
-                                if let nonce = fid.nonce,
-                                   let timestamp = fid.timestamp,
-                                   let signature = fid.signature {
-                                    imp.adImpressionIdentifier = nonce.uuidString
-                                    imp.timestamp = timestamp
-                                    imp.signature = signature
-                                    
-                                    return imp
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        guard let fidelity = SkadnParametersManager.getFidelity(from: skadnInfo, fidelityType: 0) else { return nil }
+        
+        let imp = SKAdImpression()
+        if let itunesitem = skadnInfo.itunesitem,
+           let network = skadnInfo.network,
+           let campaign = skadnInfo.campaign,
+           let sourceapp = skadnInfo.sourceapp,
+           let nonce = fidelity.nonce,
+           let timestamp = fidelity.timestamp,
+           let signature = fidelity.signature {
+            imp.sourceAppStoreItemIdentifier = sourceapp
+            imp.advertisedAppStoreItemIdentifier = itunesitem
+            imp.adNetworkIdentifier = network
+            imp.adCampaignIdentifier = campaign
+            imp.adImpressionIdentifier = nonce.uuidString
+            imp.timestamp = timestamp
+            imp.signature = signature
         }
         return nil
     }
@@ -94,20 +82,15 @@ public class SkadnParametersManager: NSObject {
                 
                 // >= SKAdNetwork 2.2
                 if #available(iOS 14.5, *) {
-                    if let safeFidelities = skadnInfo.fidelities {
-                        for fid in safeFidelities {
-                            if fid.fidelity == 0 {
-                                if let timestamp = fid.timestamp,
-                                   let nonce = fid.nonce,
-                                   let signature = fid.signature {
-                                    productParams[SKStoreProductParameterAdNetworkTimestamp] = timestamp
-                                    productParams[SKStoreProductParameterAdNetworkNonce] = nonce
-                                    productParams[SKStoreProductParameterAdNetworkAttributionSignature] = signature
-                                    
-                                    return productParams
-                                }
-                            }
-                        }
+                    guard let fidelity = SkadnParametersManager.getFidelity(from: skadnInfo, fidelityType: 1) else { return nil }
+                    if let timestamp = fidelity.timestamp,
+                       let nonce = fidelity.nonce,
+                       let signature = fidelity.signature {
+                        productParams[SKStoreProductParameterAdNetworkTimestamp] = timestamp
+                        productParams[SKStoreProductParameterAdNetworkNonce] = nonce
+                        productParams[SKStoreProductParameterAdNetworkAttributionSignature] = signature
+                        
+                        return productParams
                     }
                 }
             }
