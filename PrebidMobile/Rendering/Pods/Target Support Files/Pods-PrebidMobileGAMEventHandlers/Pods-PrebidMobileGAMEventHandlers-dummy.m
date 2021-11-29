5aeae1ca8cdb9671ea115df39ab8b9b7125820a9
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2021-11-29 15:40:51 +0200

    refactor(sdk): remove rendering artifacts

--- PrebidMobile/Rendering/Pods/Target Support Files/Pods-PrebidMobileGAMEventHandlers/Pods-PrebidMobileGAMEventHandlers-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PrebidMobileGAMEventHandlers : NSObject
-@end
-@implementation PodsDummy_Pods_PrebidMobileGAMEventHandlers
-@end
--- PrebidMobile/Rendering/Pods/Target Support Files/Pods-PrebidMobileGAMEventHandlersTests/Pods-PrebidMobileGAMEventHandlersTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PrebidMobileGAMEventHandlersTests : NSObject
-@end
-@implementation PodsDummy_Pods_PrebidMobileGAMEventHandlersTests
-@end
