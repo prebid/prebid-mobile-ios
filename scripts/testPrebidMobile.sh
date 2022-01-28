159eb77f822e487df67047122d15464a6de2b4ab
Yuriy Velichko <yuriy.velichko@openx.com>
2022-01-28 15:20:37 +0200

    skip tests for ios 13

--- scripts/testPrebidMobile.sh
+++ scripts/testPrebidMobile.sh
@@ -18,15 +18,15 @@ gem install xcpretty-travis-formatter --user-install
 gem install cocoapods --user-install
 pod install --repo-update
 
-echo -e "\n${GREEN}Running some unit tests for iOS 13${NC} \n"
-xcodebuild test -workspace PrebidMobile.xcworkspace  -scheme "PrebidMobileTests" -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.7' -only-testing PrebidMobileTests/RequestBuilderTests/testPostData | xcpretty -f `xcpretty-travis-formatter` --color --test
+# echo -e "\n${GREEN}Running some unit tests for iOS 13${NC} \n"
+# xcodebuild test -workspace PrebidMobile.xcworkspace  -scheme "PrebidMobileTests" -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.7' -only-testing PrebidMobileTests/RequestBuilderTests/testPostData | xcpretty -f `xcpretty-travis-formatter` --color --test
 
-if [[ ${PIPESTATUS[0]} == 0 ]]; then
-    echo "✅ unit tests for iOS 13 Passed"
-else
-    echo "🔴 unit tests for iOS 13 Failed"
-    exit 1
-fi
+# if [[ ${PIPESTATUS[0]} == 0 ]]; then
+#     echo "✅ unit tests for iOS 13 Passed"
+# else
+#     echo "🔴 unit tests for iOS 13 Failed"
+#     exit 1
+# fi
 
 echo -e "\n${GREEN}Running PrebidMobile unit tests${NC} \n"
 xcodebuild test -workspace PrebidMobile.xcworkspace  -scheme "PrebidMobileTests" -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=latest' | xcpretty -f `xcpretty-travis-formatter` --color --test
