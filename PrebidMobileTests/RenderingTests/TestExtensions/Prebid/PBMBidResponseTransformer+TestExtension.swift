58e6dcaae3ca1ff920ce5d8861e19090d4688433
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2022-05-10 09:34:37 +0300

    feat: update test

--- PrebidMobileTests/RenderingTests/TestExtensions/Prebid/PBMBidResponseTransformer+TestExtension.swift
+++ PrebidMobileTests/RenderingTests/TestExtensions/Prebid/PBMBidResponseTransformer+TestExtension.swift
@@ -42,12 +42,12 @@ extension PBMBidResponseTransformer {
     }
     
     static var noWinningBidResponse: PBMServerResponse {
-        return makeValidResponse(bidPrice: 0.1091000000051168, noCacheID: true)
+        return makeValidResponse(bidPrice: 0.1091000000051168, noWinningBidProperties: true)
     }
     
-    static func makeValidResponse(bidPrice: Float, noCacheID: Bool = false) -> PBMServerResponse {
-        let cacheIdFragment = noCacheID ? "" : "\"hb_cache_id\":\"bb708664-e231-4bc8-ad8a-d7accaa7d129\","
-        return buildResponse("{\"id\":\"B4A2D3F4-41B6-4D37-B68B-EE8893E85C31\",\"seatbid\":[{\"bid\":[{\"id\":\"test-bid-id-1\",\"impid\":\"8BBB0D42-5A73-45AC-B275-51B299A74C32\",\"price\":\(bidPrice),\"adm\":\"<html><div>You Won! This is a test bid</div></html>\",\"adid\":\"test-ad-id-12345\",\"adomain\":[\"openx.com\"],\"crid\":\"test-creative-id-1\",\"w\":300,\"h\":250,\"ext\":{\"prebid\":{\"cache\":{\"key\":\"\",\"url\":\"\",\"bids\":{\"url\":\"prebid.devint.openx.net/cache?uuid=bb708664-e231-4bc8-ad8a-d7accaa7d129\",\"cacheId\":\"bb708664-e231-4bc8-ad8a-d7accaa7d129\"}},\"targeting\":{\"hb_bidder\":\"openx\",\"hb_bidder_openx\":\"openx\",\"hb_cache_host\":\"prebid.devint.openx.net\",\"hb_cache_host_openx\":\"prebid.devint.openx.net\",\(cacheIdFragment)\"hb_cache_id_openx\":\"bb708664-e231-4bc8-ad8a-d7accaa7d129\",\"hb_cache_path\":\"/cache\",\"hb_cache_path_openx\":\"/cache\",\"hb_env\":\"mobile-app\",\"hb_env_openx\":\"mobile-app\",\"hb_pb\":\"0.10\",\"hb_pb_openx\":\"0.10\",\"hb_size\":\"300x250\",\"hb_size_openx\":\"300x250\"},\"type\":\"banner\"},\"bidder\":{\"ad_ox_cats\":[2],\"agency_id\":\"agency_10\",\"brand_id\":\"brand_10\",\"buyer_id\":\"buyer_10\",\"matching_ad_id\":{\"campaign_id\":1,\"creative_id\":3,\"placement_id\":2},\"next_highest_bid_price\":0.099}}}],\"seat\":\"openx\"}],\"cur\":\"USD\",\"ext\":{\"responsetimemillis\":{\"openx\":62}}}")
+    static func makeValidResponse(bidPrice: Float, noWinningBidProperties: Bool = false) -> PBMServerResponse {
+        let winningBidFragment = noWinningBidProperties ? "" : "{\"bid\":[{\"id\":\"test-bid-id-1\",\"impid\":\"8BBB0D42-5A73-45AC-B275-51B299A74C32\",\"price\":\(bidPrice),\"adm\":\"<html><div>You Won! This is a test bid</div></html>\",\"adid\":\"test-ad-id-12345\",\"adomain\":[\"openx.com\"],\"crid\":\"test-creative-id-1\",\"w\":300,\"h\":250,\"ext\":{\"prebid\":{\"targeting\":{\"hb_bidder\":\"openx\",\"hb_bidder_openx\":\"openx\",\"hb_env\":\"mobile-app\",\"hb_env_openx\":\"mobile-app\",\"hb_pb\":\"0.10\",\"hb_pb_openx\":\"0.10\",\"hb_size\":\"300x250\",\"hb_size_openx\":\"300x250\"},\"type\":\"banner\"},\"bidder\":{\"ad_ox_cats\":[2],\"agency_id\":\"agency_10\",\"brand_id\":\"brand_10\",\"buyer_id\":\"buyer_10\",\"matching_ad_id\":{\"campaign_id\":1,\"creative_id\":3,\"placement_id\":2},\"next_highest_bid_price\":0.099}}}],\"seat\":\"openx\"}"
+        return buildResponse("{\"id\":\"B4A2D3F4-41B6-4D37-B68B-EE8893E85C31\",\"seatbid\":[\(winningBidFragment)],\"cur\":\"USD\",\"ext\":{\"responsetimemillis\":{\"openx\":62}}}")
     }
     
     // MARK: - Private Helpers
