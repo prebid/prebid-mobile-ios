47d73f816c1dd002e3ed2d7c7c069cb91fa75b09
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2022-11-08 10:39:38 +0200

    feat: add new PrebidDemoSwift target

---
+++ Example/PrebidDemo/PrebidDemoSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PrebidDemoSwift
+//
+//  Created by Olena Stepaniuk on 08.11.2022.
+//  Copyright © 2022 Prebid. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
+
---
+++ Example/PrebidDemo/PrebidDemoSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrebidDemoSwift
+//
+//  Created by Olena Stepaniuk on 08.11.2022.
+//  Copyright © 2022 Prebid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
