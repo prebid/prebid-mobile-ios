f0eb9fe4947e67c24ba2be370452ea7ba10731e5
MaksPetrukhnoPI <maxim.petrukhno@postindustria.com>
2022-11-29 16:08:28 +0100

    Update RewardedVideoAdsTest.swift

--- Example/PrebidDemo/PrebidDemoSwiftUITests/Video/RewardedVideoAdsTest.swift
+++ Example/PrebidDemo/PrebidDemoSwiftUITests/Video/RewardedVideoAdsTest.swift
@@ -9,30 +9,30 @@
 import Foundation
 import XCTest
 
-//class RewardedVideoAds: BaseAdsTest {
-//    
-//    public func testInAppVideoRewardedAd() {
-//        testAd(testCase: testCases.inAppVideoRewardedCase)
-//    }
-//    public func testGamOriginalVideoRewardedAd() {
-//        testAd(testCase: testCases.gamOriginalVideoRewardedCase)
-//    }
-//    public func testGamRenderingVideoRewardedAd() {
-//        testAd(testCase: testCases.gamVideoRewardedCase)
-//    }
-//    public func testAdMobVideoRewardedAd() {
-//        testAd(testCase: testCases.adMobVideoRewardedCase)
-//    }
-//    
-//    override func checkAd(testCase: String) {
-//        if testCase == testCases.gamOriginalVideoRewardedCase {
-//            XCTAssert(app.webViews.element.waitForExistence(timeout: 30),assertFailedMessage(testCase: testCase, reason: "Video is not displayed"))
-//            XCTAssert(app.buttons["Close Advertisement"].waitForExistence(timeout: 15),assertFailedMessage(testCase: testCase, reason: "Close Button is not displayed"))
-//            XCTAssert(app.images.element.waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "End card is not displayed"))
-//        } else {
-//            XCTAssert(app.otherElements["PBMVideoView"].waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "Video is not displayed"))
-//            XCTAssert(app.webViews["PBMInternalWebViewAccessibilityIdentifier"].waitForExistence(timeout: 30),assertFailedMessage(testCase: testCase, reason: "End card is not displayed"))
-//            XCTAssert(app.buttons["PBMCloseButton"].waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "Video close button is not displayed"))
-//        }
-//    }
-//}
+class RewardedVideoAds: BaseAdsTest {
+    
+    public func testInAppVideoRewardedAd() {
+        testAd(testCase: testCases.inAppVideoRewardedCase)
+    }
+    public func testGamOriginalVideoRewardedAd() {
+        testAd(testCase: testCases.gamOriginalVideoRewardedCase)
+    }
+    public func testGamRenderingVideoRewardedAd() {
+        testAd(testCase: testCases.gamVideoRewardedCase)
+    }
+    public func testAdMobVideoRewardedAd() {
+        testAd(testCase: testCases.adMobVideoRewardedCase)
+    }
+    
+    override func checkAd(testCase: String) {
+        if testCase == testCases.gamOriginalVideoRewardedCase {
+            XCTAssert(app.webViews.element.waitForExistence(timeout: 30),assertFailedMessage(testCase: testCase, reason: "Video is not displayed"))
+            XCTAssert(app.buttons["Close Advertisement"].waitForExistence(timeout: 15),assertFailedMessage(testCase: testCase, reason: "Close Button is not displayed"))
+            XCTAssert(app.images.element.waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "End card is not displayed"))
+        } else {
+            XCTAssert(app.otherElements["PBMVideoView"].waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "Video is not displayed"))
+            XCTAssert(app.webViews["PBMInternalWebViewAccessibilityIdentifier"].waitForExistence(timeout: 30),assertFailedMessage(testCase: testCase, reason: "End card is not displayed"))
+            XCTAssert(app.buttons["PBMCloseButton"].waitForExistence(timeout: 20),assertFailedMessage(testCase: testCase, reason: "Video close button is not displayed"))
+        }
+    }
+}
