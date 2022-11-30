aca34ef05afcd36f1c4bd72896874eee4e739d12
MaksPetrukhnoPI <maxim.petrukhno@postindustria.com>
2022-11-30 15:35:06 +0100

    clean, reformating

--- Example/PrebidDemo/PrebidDemoSwiftUITests/BaseAdsTest.swift
+++ Example/PrebidDemo/PrebidDemoSwiftUITests/BaseAdsTest.swift
@@ -1,12 +1,20 @@
-//
-//  BaseAdsTest.swift
-//  PrebidDemoTests
-//
-//  Created by mac-admin on 09.11.2022.
-//  Copyright © 2022 Prebid. All rights reserved.
-//
+/*   Copyright 2018-2021 Prebid.org, Inc.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  */
 
 import XCTest
+
 class BaseAdsTest: XCTestCase {
     
     let app = XCUIApplication()
@@ -17,16 +25,13 @@ class BaseAdsTest: XCTestCase {
         app.launchArguments = ["-integrationKindAll"]
     }
     
-    override func tearDownWithError() throws {
-        
-    }
-    
     func testAd(testCase: String) {
         goToAd(testCase: testCase)
         checkAd(testCase: testCase)
     }
+    
     func checkAd(testCase: String) {}
-
+    
     func assertFailedMessage(testCase: String, reason: String) -> String {
         return "Ad Failed \(testCase): \(reason)"
     }
@@ -37,5 +42,5 @@ class BaseAdsTest: XCTestCase {
         app.searchFields.element.typeText(testCase)
         app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
     }
-
+    
 }
--- Example/PrebidDemo/PrebidDemoSwiftUITests/Video/InterstitialVideoAdsTest.swift
+++ Example/PrebidDemo/PrebidDemoSwiftUITests/Video/InterstitialVideoAdsTest.swift
@@ -1,10 +1,17 @@
-//
-//  InterstitialVideoAds.swift
-//  PrebidDemoSwiftUITests
-//
-//  Created by mac-admin on 28.11.2022.
-//  Copyright © 2022 Prebid. All rights reserved.
-//
+/*   Copyright 2018-2021 Prebid.org, Inc.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+  http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  */
 
 import Foundation
 import XCTest
@@ -14,12 +21,15 @@ class InterstitialVideoAds: BaseAdsTest {
     public func testInAppVideoInterstitialAd() {
         testAd(testCase: testCases.inAppVideoInterstitialCase)
     }
-//    public func testGamOriginalVideoInterstitialAd() {
-//        testAd(testCase: testCases.gamOriginalVideoInterstitialCase)
-//    }
+    
+    //    public func testGamOriginalVideoInterstitialAd() {
+    //        testAd(testCase: testCases.gamOriginalVideoInterstitialCase)
+    //    }
+    
     public func testGamRenderingVideoInterstitialAd() {
         testAd(testCase: testCases.gamVideoInterstitialCase)
     }
+    
     public func testAdMobVideoInterstitialAd() {
         testAd(testCase: testCases.adMobVideoInterstitialCase)
     }
