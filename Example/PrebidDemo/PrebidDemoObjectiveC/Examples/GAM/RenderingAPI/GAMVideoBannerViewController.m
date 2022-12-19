b0192aa86ddfc7703358e21fd24baa1da4538fc9
MaksPetrukhnoPI <maxim.petrukhno@postindustria.com>
2022-12-19 15:51:30 +0100

    fixed video banner in objective c demo

--- Example/PrebidDemo/PrebidDemoObjectiveC/Examples/GAM/RenderingAPI/GAMVideoBannerViewController.m
+++ Example/PrebidDemo/PrebidDemoObjectiveC/Examples/GAM/RenderingAPI/GAMVideoBannerViewController.m
@@ -40,7 +40,7 @@ NSString * const gamAdUnitVideoRendering = @"/21808260008/prebid_oxb_300x250_ban
 
 - (void)createAd {
     // 1. Create a GAMBannerEventHandler
-    GAMBannerEventHandler * eventHandler = [[GAMBannerEventHandler alloc] initWithAdUnitID:gamAdUnitVideoRendering validGADAdSizes:@[NSValueFromGADAdSize(GADAdSizeBanner)]];
+    GAMBannerEventHandler * eventHandler = [[GAMBannerEventHandler alloc] initWithAdUnitID:gamAdUnitVideoRendering validGADAdSizes:@[NSValueFromGADAdSize(GADAdSizeMediumRectangle)]];
     
     // 2. Create a BannerView
     self.prebidBannerView = [[BannerView alloc] initWithFrame:CGRectMake(0, 0, self.adSize.width, self.adSize.height) configID:storedImpVideoGAMRendering adSize:self.adSize eventHandler:eventHandler];
