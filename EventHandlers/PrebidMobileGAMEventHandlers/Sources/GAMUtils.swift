45dc0f89ecdf99cc0681e0df02cf62262bd2b9ab
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2023-01-27 16:06:11 +0200

    feat: update supported versions

--- EventHandlers/PrebidMobileGAMEventHandlers/Sources/GAMUtils.swift
+++ EventHandlers/PrebidMobileGAMEventHandlers/Sources/GAMUtils.swift
@@ -59,7 +59,7 @@ public class GAMUtils: NSObject {
     
     // Added for tests
     static func latestTestedGMAVersion() -> GADVersionNumber {
-        return GADVersionNumber(majorVersion: 9, minorVersion: 14, patchVersion: 0)
+        return GADVersionNumber(majorVersion: 10, minorVersion: 0, patchVersion: 0)
     }
     
     // MARK: Private Methods
