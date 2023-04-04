b50b9eb40a31658226bb693ca8abbfe2c89331fc
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2023-04-04 17:19:15 +0300

    feat: deprecate video ad unit

---
+++ PrebidMobile/AdUnits/VideoAdUnit.swift
@@ -0,0 +1,32 @@
+/*   Copyright 2018-2019 Prebid.org, Inc.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+
+@available(*, deprecated, message: "This class is deprecated.")
+public class VideoAdUnit: VideoBaseAdUnit {
+    
+    public init(configId: String, size: CGSize) {
+        super.init(configId: configId, size: size)
+        adUnitConfig.adFormats = [.video]
+    }
+    
+    public func addAdditionalSize(sizes: [CGSize]) {
+        if super.adUnitConfig.additionalSizes == nil {
+            super.adUnitConfig.additionalSizes = [CGSize]()
+        }
+        super.adUnitConfig.additionalSizes?.append(contentsOf: sizes)
+    }
+}
