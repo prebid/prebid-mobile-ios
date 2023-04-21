eba6776671bc8f0c18e930cf63bcb3f6047a9d6e
Olena Stepaniuk <92721590+OlenaPostindustria@users.noreply.github.com>
2023-04-21 08:57:51 +0300

    Deprecate `.display` AdFormat (#824)
    * feat: revert deleting display AdFormat & deprecate display AdFormat
    
    * clean: lowercase
    
    * test: update test

--- PrebidMobileTests/RenderingTests/Tests/ParameterBuilderTests/PrebidParameterBuilderTest.swift
+++ PrebidMobileTests/RenderingTests/Tests/ParameterBuilderTests/PrebidParameterBuilderTest.swift
@@ -661,6 +661,22 @@ class PrebidParameterBuilderTest: XCTestCase {
             XCTAssertEqual($0.banner?.api, [3, 5, 6, 7, 4, 1, 2, 4])
         }
     }
+    
+    func testBannerParameters_deprecatedDisplayFormat() {
+        // Original API
+        let adUnit = AdUnit(configId: "test", size: CGSize(width: 320, height: 50), adFormats: [.display])
+
+        let bannerParameters = BannerParameters()
+        bannerParameters.api = [.MRAID_1, .MRAID_2, .MRAID_3, .OMID_1, .ORMMA, .VPAID_1, .VPAID_2, .ORMMA]
+
+        adUnit.adUnitConfig.adConfiguration.bannerParameters = bannerParameters
+
+        let bidRequest = buildBidRequest(with: adUnit.adUnitConfig)
+
+        bidRequest.imp.forEach {
+            XCTAssertEqual($0.banner?.api, [3, 5, 6, 7, 4, 1, 2, 4])
+        }
+    }
 
     func testVideoParameters() {
         // Original API
