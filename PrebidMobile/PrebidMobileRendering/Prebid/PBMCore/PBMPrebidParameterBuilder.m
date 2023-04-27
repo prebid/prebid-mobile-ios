e8e35c486355e0e43b0f857ac46ee9af0f068260
Mark Birch <mark.birch@gmail.com>
2023-04-27 12:55:21 +0100

    changing customData to be an array

--- PrebidMobile/PrebidMobileRendering/Prebid/PBMCore/PBMPrebidParameterBuilder.m
+++ PrebidMobile/PrebidMobileRendering/Prebid/PBMCore/PBMPrebidParameterBuilder.m
@@ -63,51 +63,51 @@
 }
 
 - (void)buildBidRequest:(nonnull PBMORTBBidRequest *)bidRequest {
-    
+
     NSSet<AdFormat *> *adFormats = self.adConfiguration.adConfiguration.adFormats;
     BOOL const isHTML = ([adFormats containsObject:AdFormat.display]);
     BOOL const isInterstitial = self.adConfiguration.adConfiguration.isInterstitialAd;
-    
+
     bidRequest.requestID = [NSUUID UUID].UUIDString;
     bidRequest.extPrebid.storedRequestID        = self.sdkConfiguration.prebidServerAccountId;
     bidRequest.extPrebid.storedAuctionResponse  = Prebid.shared.storedAuctionResponse;
     bidRequest.extPrebid.dataBidders            = self.targeting.accessControlList;
     bidRequest.extPrebid.storedBidResponses     = [Prebid.shared getStoredBidResponses];
-    
+
     if (Prebid.shared.useCacheForReportingWithRenderingAPI) {
         PBMMutableJsonDictionary * const cache = [PBMMutableJsonDictionary new];
         cache[@"bids"] = [PBMMutableJsonDictionary new];
         cache[@"vastxml"] = [PBMMutableJsonDictionary new];
         bidRequest.extPrebid.cache = cache;
     }
-    
+
     bidRequest.app.publisher.publisherID        = self.sdkConfiguration.prebidServerAccountId;
     bidRequest.app.ver          = [NSBundle mainBundle].infoDictionary[@"CFBundleShortVersionString"];
     bidRequest.device.pxratio   = @([UIScreen mainScreen].scale);
     bidRequest.source.tid       = [NSUUID UUID].UUIDString;
     bidRequest.device.ua        = [self.userAgentService getFullUserAgent];
-    
+
     bidRequest.app.content = [self.adConfiguration getAppContent];
-    
+
     if (self.targeting.userDataDictionary.count > 0) {
         bidRequest.user.ext[@"data"] = self.targeting.userDataDictionary;
     }
-    
+
     if (self.targeting.gdprConsentString && self.targeting.gdprConsentString.length > 0) {
         bidRequest.user.ext[@"consent"] = self.targeting.gdprConsentString;
     }
 
     PBMORTBSourceExtOMID *extSource = [PBMORTBSourceExtOMID new];
-    
+
     if (!self.adConfiguration.adConfiguration.isOriginalAPI) {
         extSource.omidpn = @"Prebid";
         extSource.omidpv = [PBMFunctions omidVersion];
     }
-    
+
     if (Targeting.shared.omidPartnerName) {
         extSource.omidpn = Targeting.shared.omidPartnerName;
     }
-    
+
     if (Targeting.shared.omidPartnerVersion) {
         extSource.omidpv = Targeting.shared.omidPartnerVersion;
     }
@@ -116,7 +116,7 @@
 
     NSArray<PBMORTBFormat *> *formats = nil;
     const NSInteger formatsCount = (CGSizeEqualToSize(self.adConfiguration.adSize, CGSizeZero) ? 0 : 1) + self.adConfiguration.additionalSizes.count;
-    
+
     if (formatsCount > 0) {
         NSMutableArray<PBMORTBFormat *> * const newFormats = [[NSMutableArray alloc] initWithCapacity:formatsCount];
         if (!CGSizeEqualToSize(self.adConfiguration.adSize, CGSizeZero)) {
@@ -143,126 +143,126 @@
             interstitial.minheightperc = @(minSizePerc.height);
         }
     }
-    
+
     NSArray<PBMORTBContentData *> *userData = [self.adConfiguration getUserData];
     if (userData) {
         bidRequest.user.data = userData;
     }
-    
+
     PBMORTBAppExt * const appExt = bidRequest.app.ext;
     PBMORTBAppExtPrebid * const appExtPrebid = appExt.prebid;
-    
+
     if (self.targeting.contextDataDictionary.count > 0) {
         appExt.data = self.targeting.contextDataDictionary;
     }
-    
+
     for (PBMORTBImp *nextImp in bidRequest.imp) {
         nextImp.impID = [NSUUID UUID].UUIDString;
         nextImp.extPrebid.storedRequestID = self.adConfiguration.configId;
         nextImp.extPrebid.storedAuctionResponse = Prebid.shared.storedAuctionResponse;
         nextImp.extPrebid.isRewardedInventory = self.adConfiguration.adConfiguration.isOptIn;
-        
+
         /*
          in adConfiguration:
          public var ozoneAdUnitCode: String = "No adunit code set"
          public var ozoneCustomData: [AnyHashable: Any]? = nil
          public var ozoneTransactionId: String = UUID.init().uuidString
          */
-        
-        nextImp.extOzoneData = [[NSMutableDictionary alloc] initWithDictionary:
-                                @{ @"adUnitCode": self.adConfiguration.ozoneAdUnitCode,
-                                   @"customData": @{
-                                      @"settings": @{},
-                                      @"targeting": self.adConfiguration.ozoneCustomDataTargeting},
-                                  @"transactionId": self.adConfiguration.ozoneTransactionId
-                                }];
-        
+
+         nextImp.extOzoneData = [[NSMutableDictionary alloc] initWithDictionary:
+                                 @{ @"adUnitCode": self.adConfiguration.ozoneAdUnitCode,
+                                    @"customData": [NSArray arrayWithObject: @{
+                                       @"settings": @{},
+                                       @"targeting": self.adConfiguration.ozoneCustomDataTargeting}],
+                                   @"transactionId": self.adConfiguration.ozoneTransactionId
+                                 }];
+
         if (self.adConfiguration.contextDataDictionary.count > 0) {
             nextImp.extContextData = self.adConfiguration.contextDataDictionary.mutableCopy;
         }
-        
+
         nextImp.extContextData[@"adslot"] = [self.adConfiguration getPbAdSlot];
-       
+
         for (AdFormat* adFormat in adFormats) {
             if (adFormat == AdFormat.display) {
                 PBMORTBBanner * const nextBanner = nextImp.banner;
                 if (formats) {
                     nextBanner.format = formats;
                 }
-                
+
                 BannerParameters *bannerParameters = self.adConfiguration.adConfiguration.bannerParameters;
-                
+
                 if (bannerParameters.api && bannerParameters.api.count > 0) {
                     nextBanner.api = bannerParameters.rawAPI;
                 }
-                
+
                 if (self.adConfiguration.adPosition != AdPositionUndefined) {
                     nextBanner.pos = @(self.adConfiguration.adPosition);
                 }
             } else if (adFormat == AdFormat.video) {
                 PBMORTBVideo * const nextVideo = nextImp.video;
-                
+
                 if (!self.adConfiguration.adConfiguration.isOriginalAPI) {
                     nextVideo.playbackend = @(2);
                     nextVideo.pos = @(7);
                     nextVideo.protocols = @[@(2),@(5)];
                     nextVideo.mimes = PBMConstants.supportedVideoMimeTypes;
                 }
-                
+
                 nextVideo.delivery = @[@(3)];
-                
+
                 if (formats.count) {
                     PBMORTBFormat * const primarySize = (PBMORTBFormat *)formats[0];
                     nextVideo.w = primarySize.w;
                     nextVideo.h = primarySize.h;
                 }
-                
+
                 VideoParameters *videoParameters = self.adConfiguration.adConfiguration.videoParameters;
-                                
+
                 if (videoParameters.api && videoParameters.api.count > 0) {
                     nextVideo.api = videoParameters.rawAPI;
                 }
-                
+
                 if (videoParameters.maxBitrate) {
                     nextVideo.maxbitrate = [NSNumber numberWithInteger:videoParameters.maxBitrate.value];
                 }
-                
+
                 if (videoParameters.minBitrate) {
                     nextVideo.minbitrate = [NSNumber numberWithInteger:videoParameters.minBitrate.value];
                 }
-                
+
                 if (videoParameters.maxDuration) {
                     nextVideo.maxduration = [NSNumber numberWithInteger:videoParameters.maxDuration.value];
                 }
-                
+
                 if (videoParameters.minDuration) {
                     nextVideo.minduration = [NSNumber numberWithInteger:videoParameters.minDuration.value];
                 }
-                
+
                 if (videoParameters.mimes && videoParameters.mimes.count > 0) {
                     nextVideo.mimes = videoParameters.mimes;
                 }
-                
+
                 if (videoParameters.playbackMethod && videoParameters.playbackMethod.count > 0) {
                     nextVideo.playbackmethod = videoParameters.rawPlaybackMethod;
                 }
-                
+
                 if (videoParameters.protocols && videoParameters.protocols.count > 0) {
                     nextVideo.protocols = videoParameters.rawProtocols;
                 }
-                
+
                 if (videoParameters.startDelay) {
                     nextVideo.startdelay = [NSNumber numberWithInteger:videoParameters.startDelay.value];
                 }
-                
+
                 if (videoParameters.placement) {
                     nextVideo.placement = [NSNumber numberWithInteger:videoParameters.placement.value];
                 }
-                
+
                 if (videoParameters.linearity) {
                     nextVideo.linearity = [NSNumber numberWithInteger:videoParameters.linearity.value];
                 }
-                
+
                 if (self.adConfiguration.adPosition != AdPositionUndefined) {
                     nextVideo.pos = @(self.adConfiguration.adPosition);
                 }
@@ -275,15 +275,15 @@
                 }
             }
         }
-        
+
         if (isInterstitial) {
             nextImp.instl = @(1);
         }
-        
+
         if (!appExtPrebid.source) {
             appExtPrebid.source = @"prebid-mobile";
         }
-        
+
         if (!appExtPrebid.version) {
             appExtPrebid.version = Prebid.shared.version;
         }
