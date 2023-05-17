53f6eabf3f893b24877da1ea0f8afff52fa74034
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2023-05-17 10:53:19 +0300

    feat: update supported versions

--- PrebidMobile/AdUnits/Utils.swift
+++ PrebidMobile/AdUnits/Utils.swift
@@ -25,7 +25,7 @@ public class Utils: NSObject {
     public static let shared = Utils()
     
     var latestTestedGMAVersion: (Int, Int, Int) {
-        (10, 4, 0)
+        (10, 5, 0)
     }
 
     /**
