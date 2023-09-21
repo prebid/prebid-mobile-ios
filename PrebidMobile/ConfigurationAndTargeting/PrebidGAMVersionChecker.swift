0c986c32ff171fc6b40c90e836020ed60936b670
Olena Stepaniuk <92721590+OlenaPostindustria@users.noreply.github.com>
2023-09-21 12:55:31 +0300

    Support GMA SDK 10.10.0 (#916)

--- PrebidMobile/ConfigurationAndTargeting/PrebidGAMVersionChecker.swift
+++ PrebidMobile/ConfigurationAndTargeting/PrebidGAMVersionChecker.swift
@@ -18,7 +18,7 @@ import Foundation
 class PrebidGAMVersionChecker {
     
     var latestTestedGMAVersion: (Int, Int, Int) {
-        (10, 9, 0)
+        (10, 10, 0)
     }
     
     var currentGMAVersion: (Int, Int, Int)?
