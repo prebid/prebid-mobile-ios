cf3262eae1fb7130ee1184b18806770dd672514e
Jono Sligh <jsligh@prebid.org>
2024-04-18 12:14:12 -0500

    Needed to fix the tests now

--- scripts/buildPrebidMobile.sh
+++ scripts/buildPrebidMobile.sh
@@ -43,7 +43,6 @@ touch "$LOG_FILE_FRAMEWORK"
 echo -e "\n\n${GREEN}INSTALL PODS${NC}\n\n"
 
 export PATH="/Users/distiller/.gem/ruby/2.7.0/bin:$PATH"
-echo $(gem -v)
 gem install cocoapods
 pod install --repo-update
 
