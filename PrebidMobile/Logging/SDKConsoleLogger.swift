c81fb81b7e45031dbc45f5941ed4990230251dd5
Jono Sligh <jsligh@prebid.org>
2024-08-05 12:28:41 -0500

    Included log function in other functions.

--- PrebidMobile/Logging/SDKConsoleLogger.swift
+++ PrebidMobile/Logging/SDKConsoleLogger.swift
@@ -12,27 +12,27 @@ import Foundation
 public class SDKConsoleLogger: NSObject, PrebidLogger {
     
     public func error(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .error, filename: filename, line: line, function: function)
     }
 
     public func info(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .info, filename: filename, line: line, function: function)
     }
 
     public func debug(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .debug, filename: filename, line: line, function: function)
     }
 
     public func verbose(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .verbose, filename: filename, line: line, function: function)
     }
 
     public func warn(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .warn, filename: filename, line: line, function: function)
     }
 
     public func severe(_ object: Any, filename: String, line: Int, function: String) {
-        
+        log(object, logLevel: .severe, filename: filename, line: line, function: function)
     }
 
     public func whereAmI(filename: String, line: Int, function: String) {
