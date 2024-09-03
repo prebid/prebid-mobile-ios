29af217349b78ddf0f5a62e410390ed363c5b96e
Jono Sligh <jsligh@prebid.org>
2024-09-03 12:06:29 -0500

    Removed buyerUID

--- PrebidMobile/PrebidMobileRendering/Networking/Parameters/PBMParameterBuilderService.m
+++ PrebidMobile/PrebidMobileRendering/Networking/Parameters/PBMParameterBuilderService.m
@@ -115,7 +115,6 @@
     }
     
     bidRequest.user.gender      = targeting.userGenderDescription;
-    bidRequest.user.buyeruid    = targeting.buyerUID;
     bidRequest.user.customdata  = targeting.userCustomData;
     bidRequest.user.userid      = targeting.userID;
    
