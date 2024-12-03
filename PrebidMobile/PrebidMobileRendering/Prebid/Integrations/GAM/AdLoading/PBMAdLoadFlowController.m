23350406fc075a9002179b39af10553bb4862108
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2024-12-03 15:34:24 +0200

    fix: missing winning ad format

--- PrebidMobile/PrebidMobileRendering/Prebid/Integrations/GAM/AdLoading/PBMAdLoadFlowController.m
+++ PrebidMobile/PrebidMobileRendering/Prebid/Integrations/GAM/AdLoading/PBMAdLoadFlowController.m
@@ -275,7 +275,6 @@
     
     self.flowState = PBMAdLoadFlowState_LoadingDisplayView;
     AdUnitConfig * const adUnitConfig = self.savedAdUnitConfig;
-    adUnitConfig.adConfiguration.winningBidAdFormat = bid.adFormat;
     
     @weakify(self);
     dispatch_sync(dispatch_get_main_queue(), ^{
