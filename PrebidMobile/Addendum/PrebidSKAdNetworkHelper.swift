96cc63e2dc0e1800945ff73fc2d9a68ca0e435bb
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2025-01-14 14:55:19 +0200

    feat: support SKAdN in native

---
+++ PrebidMobile/Addendum/PrebidSKAdNetworkHelper.swift
@@ -0,0 +1,22 @@
+//
+//  PrebidSKAdNetworkHelper.swift
+//  PrebidMobile
+//
+//  Created by Olena Stepaniuk on 12.01.2025.
+//  Copyright © 2025 AppNexus. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+@objcMembers
+public class PrebidSKAdNetworkHelper: NSObject {
+    
+    public func subscribeOnAdClickedBanner(viewController: UIViewController, adView: UIView) {
+        
+    }
+    
+    public func subscribeOnAdClickedVideo(viewController: UIViewController, adView: UIView) {
+        
+    }
+}
