6eb3c92b59083801d9170b37c5cc21ba8d7edafe
Maksym Danylov <maksym.danylov@sigma.software>
2025-01-29 17:08:00 +0200

    Marks methods as deprecated.

--- PrebidMobile/ConfigurationAndTargeting/Targeting.swift
+++ PrebidMobile/ConfigurationAndTargeting/Targeting.swift
@@ -341,6 +341,8 @@ public class Targeting: NSObject {
     /// Sets custom parameters by adding each key-value pair to the parameter dictionary.
     ///
     /// - Parameter params: A dictionary of parameters to set. If `nil`, no parameters are added.
+    /// Deprecated.
+    @available(*, deprecated, message: "This method is deprecated. In the upcoming major release, the method will be removed.")
     public func setCustomParams(_ params: [String : String]?) {
         guard let params = params else {
             return
@@ -358,6 +360,8 @@ public class Targeting: NSObject {
     /// - Parameters:
     ///   - value: The value of the custom parameter.
     ///   - withName: The name of the custom parameter. If `nil`, the parameter is not added.
+    /// Deprecated.
+    @available(*, deprecated, message: "This method is deprecated. In the upcoming major release, the method will be removed.")
     public func addCustomParam(_ value: String, withName: String?) {
         guard let name = withName else {
             return
