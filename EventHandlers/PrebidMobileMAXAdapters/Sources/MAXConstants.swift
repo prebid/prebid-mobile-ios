07a4fe448f481c231d2859c879441efd0cfc2e59
Maksym Danylov <maksym.danylov@sigma.software>
2025-03-13 13:15:43 +0200

    Bump to 2.5.0

--- EventHandlers/PrebidMobileMAXAdapters/Sources/MAXConstants.swift
+++ EventHandlers/PrebidMobileMAXAdapters/Sources/MAXConstants.swift
@@ -17,5 +17,5 @@ import Foundation
 
 @objcMembers
 public class MAXConstants: NSObject {
-    public static let PrebidMAXAdapterVersion = "2.4.0"
+    public static let PrebidMAXAdapterVersion = "2.5.0"
 }
