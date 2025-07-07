5afad6f9a818884a8f7f5a885df2d52e89e8bde4
James Lou <jlou@yahooinc.com>
2025-07-07 13:35:05 -0700

    Add travis build

---
+++ scripts/testPrebidSPM.sh
@@ -0,0 +1,35 @@
+if [ -d "scripts" ]; then
+cd scripts/
+fi
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "\n\n${GREEN}RUN PREBID DEMO TESTS${NC}\n\n"
+
+echo -e "\n${GREEN}Creating simulator${NC} \n"
+xcrun simctl create iPhone-16-Pro-PrebidMobile com.apple.CoreSimulator.SimDeviceType.iPhone-16-Pro
+
+cd ..
+echo $PWD
+
+export PATH="/Users/distiller/.gem/ruby/2.7.0/bin:$PATH"
+
+brew install xcbeautify
+
+echo -e "\n${GREEN}Running integration tests${NC} \n"
+
+xcodebuild \
+    -workspace PrebidMobile.xcworkspace \
+    -scheme SPMTestApp \
+    -destination 'platform=iOS Simulator,name=iPhone-16-Pro-PrebidMobile,OS=latest' | xcbeautify
+
+if [[ ${PIPESTATUS[0]} == 0 ]]; then
+    echo "✅ Build Success"
+else
+    echo "🔴 Build Failed"
+    exit 1
+fi
+
+echo -e "\n${GREEN}Removing simulator${NC} \n"
+xcrun simctl delete iPhone-16-Pro-PrebidMobile
