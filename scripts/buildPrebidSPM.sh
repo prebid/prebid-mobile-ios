0925c69dc1dac82f2ab567931428e3eef700816f
James Lou <jlou@yahooinc.com>
2025-07-07 17:05:36 -0700

    show destinations in spm build script

--- scripts/buildPrebidSPM.sh
+++ scripts/buildPrebidSPM.sh
@@ -1,28 +1,26 @@
-if [ -d "scripts" ]; then
-cd scripts/
-fi
-
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "\n\n${GREEN}RUN PREBID DEMO TESTS${NC}\n\n"
 
-echo -e "\n${GREEN}Creating simulator${NC} \n"
-xcrun simctl create iPhone-16-Pro-PrebidMobile-SPM com.apple.CoreSimulator.SimDeviceType.iPhone-16-Pro
-
-cd ..
 echo $PWD
 
-export PATH="/Users/distiller/.gem/ruby/2.7.0/bin:$PATH"
-
 brew install xcbeautify
+
+echo -e "\n${GREEN}Creating simulator${NC} \n"
+xcrun simctl create iPhone-16-Pro-PrebidMobile com.apple.CoreSimulator.SimDeviceType.iPhone-16-Pro
+
+xcodebuild \
+    -workspace PrebidMobile.xcworkspace \
+    -scheme SPMTestApp \
+    -showdestinations
 
 echo -e "\n${GREEN}Running integration tests${NC} \n"
 
 xcodebuild \
     -workspace PrebidMobile.xcworkspace \
     -scheme SPMTestApp \
-    -destination 'platform=iOS Simulator,name=iPhone-16-Pro-PrebidMobile-SPM,OS=latest' | xcbeautify
+    -destination 'platform=iOS Simulator,OS=latest' | xcbeautify
 
 if [[ ${PIPESTATUS[0]} == 0 ]]; then
     echo "✅ Build Success"
@@ -32,4 +30,4 @@ else
 fi
 
 echo -e "\n${GREEN}Removing simulator${NC} \n"
-xcrun simctl delete iPhone-16-Pro-PrebidMobile-SPM
+xcrun simctl delete iPhone-16-Pro-PrebidMobile
