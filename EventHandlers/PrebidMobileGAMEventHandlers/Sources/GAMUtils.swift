3f93e7ce82ca87c83f2669fbd7f8edcebac650c1
Maksym Danylov <146753616+mdanylov-sigma@users.noreply.github.com>
2026-08-14 21:00:54 +0300

    Bumps to 3.3.4 (#1315)

--- EventHandlers/PrebidMobileGAMEventHandlers/Sources/GAMUtils.swift
+++ EventHandlers/PrebidMobileGAMEventHandlers/Sources/GAMUtils.swift
@@ -80,7 +80,7 @@ public class GAMUtils: NSObject {
     static func latestTestedGMAVersion() -> GoogleMobileAds.VersionNumber {
         GoogleMobileAds.VersionNumber(
             majorVersion: 13,
-            minorVersion: 6,
+            minorVersion: 7,
             patchVersion: 0
         )
     }
